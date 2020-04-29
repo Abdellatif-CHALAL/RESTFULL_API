2e458477a16096a2286e668dd72ed3d416c0e13b
CHALAL <abdellatifchalal44@gmail.com>
2020-04-29 14:57:13 +0200

    RESTFULL_API

---
+++ app/Http/Controllers/Seller/SellerTransactionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Seller;
+use Illuminate\Http\Request;
+
+class SellerTransactionController extends ApiController
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->middleware('scope:read-general')->only('index');
+        $this->middleware('can:view,seller')->only('index');
+
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $transactions = $seller->products()
+        ->whereHas('Transactions')
+        ->with('Transactions')
+        ->get()
+        ->pluck('Transactions')
+        ->collapse();
+        return $this->showAll($transactions);
+    }
+
+    
+}
